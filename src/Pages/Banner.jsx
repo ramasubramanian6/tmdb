d4b0d30cdb272aecccaa9779295cd37e0a6f649e
ramasubramanian6 <ramsubramanianponni37@gmail.com>
2024-08-03 07:33:10 +0530

    changes on template and for mobile use

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -32,7 +32,7 @@ import { useEffect, useState } from 'react'
     <div className="flex flex-col justify-center mx-auto  w-11/12 relative">
       <img src={`https://image.tmdb.org/t/p/original/${banner}`} alt="Banner Image"  className=' rounded-2xl mt-5 ' />
       <div >
-        <div className='flex h-[5.5rem] pt-2 bg-opacity-30 bg-white justify-center  md:text-6xl absolute left-0 right-0 bottom-5 font-title text-white text-opacity-80'>#1  {title}</div>  
+        <div className='flex h-9 md:h-[5.5rem] pt-2 bg-opacity-30 bg-white justify-center  md:text-6xl absolute left-0 right-0 bottom-5 font-title text-white text-opacity-80'>#1  {title}</div>  
       </div>
     </div>
     </div>
--- src/Pages/Top10.jsx
+++ src/Pages/Top10.jsx
@@ -38,10 +38,10 @@ function Top10({movie,addWatch,RemoveWatch,ismovielike,handletoggle }) {
   return (
     <div>
             
-            <div className='flex justify-center font-bold pt-3 text-6xl my-5 text-yellow-600 underline '>Top 20 Trending Movies</div>
-            <div className='flex  justify-end pr-12 top-10 '>
-                <button className='flex  text-white text-3xl' onClick={handletogglebutton}  >
-                <span className='pr-2 font-title text-yellow-500'>Week</span> {flag ? <FaToggleOn className=' size-[2.5rem] text-red-600'></FaToggleOn>: <FaToggleOff className=' size-[2.5rem] text-fuchsia-800'></FaToggleOff>}<span className='pl-2 font-title text-yellow-500 '>Day</span>
+            <div className='flex justify-center font-bold pt-3 text-3xl md:text-6xl my-5 text-yellow-600 underline '>Top 20 Trending Movies</div>
+            <div className='flex  justify-end md:pr-12 pr-3 top-10 '>
+                <button className='flex  text-white text-[1rem] md:text-3xl' onClick={handletogglebutton} ontouch >
+                <span className='pr-2 pt-2 md:pt-0 font-title text-yellow-500'>Week</span> {flag ? <FaToggleOn className=' size-[2.5rem] text-red-600'></FaToggleOn>: <FaToggleOff className=' size-[2.5rem] text-fuchsia-800'></FaToggleOff>}<span className='pl-2 pt-2 md:pt-0 font-title text-yellow-500 '>Day</span>
                     
                 </button>
             </div>
@@ -51,9 +51,9 @@ function Top10({movie,addWatch,RemoveWatch,ismovielike,handletoggle }) {
                 movie.map((e, index) => {
                     return (
                     <>
-                        <div key={e.id} className='m-4  hover:scale-105 relative' style={{ width: 'calc(24% - 2rem)' }}>
+                        <div key={e.id} className='m-4 hover:scale-105 relative w-[calc(50%_-_2rem)] md:w-[calc(24%_-_2rem)]'>
                         <img src={`https://image.tmdb.org/t/p/original/${e.poster_path}`} alt="Movie Banner" className=' w-full object-cover rounded-2xl  ' />
-                        <div className='h-11 text-[1.1rem] font-medium w-full  bg-slate-600 bg-opacity-40 absolute flex  justify-center  items-center font-name top-[1rem]  text-white'>{e.original_title}</div>
+                        <div className='h-11 text-[1rem]  md:text-[1.1rem] md:font-medium w-full  bg-slate-600 bg-opacity-40 absolute flex  justify-center  items-center font-name top-[1rem] md:top-[1rem]  text-white'>{e.original_title}</div>
                                                 
                         <div onClick={()=>likehandler(index)} className='bg-white bg-opacity-55 rounded-sm size-9 font-semibold hover:scale-110 absolute bottom-1 right-2 '>
                         {
